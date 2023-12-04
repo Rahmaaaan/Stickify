80641e76a01560afd6dfb1d71a37bf300ac50f95
Aqueel ur Rahman Khan <119236505+Rahmaaaan@users.noreply.github.com>
2023-12-04 17:27:37 +0530

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,48 @@
+import React, { useState, useEffect } from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Note from "./Note";
+import CreateArea from "./CreateArea";
+import axios from "axios";
+
+// Delete "https://keeper-api-azna.onrender.com"
+const backendServerURL = "https://keeper-api-azna.onrender.com" || "http://localhost:5000";
+
+function App() {
+  const [notes, setNotes] = useState([]);
+
+  useEffect(()=>{
+    axios.get(backendServerURL+"/api/getAll").then(res=> setNotes(res.data));
+  },[])
+
+  function addNote(newNote) {
+    if(newNote.title || newNote.content){
+      axios.post(backendServerURL + "/api/addNew",newNote).then(res => setNotes(res.data))
+    }
+  }
+
+  const deleteNote = (id) => {
+    axios.post(backendServerURL+"/api/delete",{id}).then(res => setNotes(res.data))
+  }
+
+  return (
+    <div>
+      <Header />
+      <CreateArea onAdd={addNote} />
+      {notes.map((noteItem, index) => {
+        return (
+          <Note
+            key={index}
+            id={noteItem._id}
+            title={noteItem.title}
+            content={noteItem.content}
+            onDelete={deleteNote}
+          />
+        );
+      })}
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
